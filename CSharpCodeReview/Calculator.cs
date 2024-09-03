2cfa59026022dbcdf6adb5e1a920ed024fa1050f
allenwa2_cisco <allenwa2@cisco.com>
2024-09-03 12:21:24 -0400

    add power function

--- CSharpCodeReview/Calculator.cs
+++ CSharpCodeReview/Calculator.cs
@@ -30,6 +30,12 @@ namespace CSharpCodeReview
             return (double)a / b;
         }
 
+        //add a power function
+        public int Power(int a, int b)
+        {
+            return (int)Math.Pow(a, b);
+        }
+
 
     }
 

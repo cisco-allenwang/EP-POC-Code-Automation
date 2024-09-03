08073f9aef00fbf25e80eca345122787f17980b9
allenwa2_cisco <allenwa2@cisco.com>
2024-09-03 12:45:34 -0400

    Code Clean Up

--- CSharpCodeReview/Calculator.cs
+++ CSharpCodeReview/Calculator.cs
@@ -30,31 +30,6 @@ namespace CSharpCodeReview
             return (double)a / b;
         }
 
-        //add a power function
-        public int Power(int a, int b)
-        {
-            return (int)Math.Pow(a, b);
-        }
-
-        //add a factorial function
-
-        public int Factorial(int n)
-        {
-            if (n < 0)
-            {
-                throw new ArgumentException("n must be a non-negative integer.");
-            }
-
-            int result = 1;
-            for (int i = 2; i <= n; i++)
-            {
-                result *= i;
-            }
-
-            return result;  //return result     
-        }
-
-
     }
 
 }

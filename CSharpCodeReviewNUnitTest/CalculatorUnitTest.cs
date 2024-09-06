0eda203fee507ccd44d2753ba62aa7781060507a
allenwa2_cisco <allenwa2@cisco.com>
2024-09-06 10:41:30 -0400

    Updated testing case

--- CSharpCodeReviewNUnitTest/CalculatorUnitTest.cs
+++ CSharpCodeReviewNUnitTest/CalculatorUnitTest.cs
@@ -53,6 +53,67 @@ using NUnit.Framework;
             // Assert
             Assert.That(result, Is.EqualTo(2.0));
         }
+
+        // Add testing cases for Calculator.cs
+        
+        [Test]
+        public void TestSubtract()
+        {
+            // Arrange
+            var calculator = new Calculator();
+            int a = 10;
+            int b = 5;
+        
+            // Act
+            int result = calculator.Subtract(a, b);
+        
+            // Assert
+            Assert.That(result, Is.EqualTo(5));
+        }
+        
+        [Test]
+        public void TestMultiply()
+        {
+            // Arrange
+            var calculator = new Calculator();
+            int a = 10;
+            int b = 5;
+        
+            // Act
+            int result = calculator.Multiply(a, b);
+        
+            // Assert
+            Assert.That(result, Is.EqualTo(50));
+        }
+        
+        [Test]
+        public void TestPower()
+        {
+            // Arrange
+            var calculator = new Calculator();
+            int a = 2;
+            int b = 3;
+        
+            // Act
+            double result = calculator.Power(a, b);
+        
+            // Assert
+            Assert.That(result, Is.EqualTo(8.0));
+        }
+        
+        [Test]
+        public void TestFactorial()
+        {
+            // Arrange
+            var calculator = new Calculator();
+            int a = 5;
+        
+            // Act
+            int result = calculator.Factorial(a);
+        
+            // Assert
+            Assert.That(result, Is.EqualTo(120));
+        }
     }
 
     
